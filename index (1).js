7ec9efaa62ecc7ad549c1698b9c942e7a9243fb4
gawayahmad123-oss <gawayahmad123@gmail.com>
2025-11-30 19:08:58 -0500

    Delete index (1).js

--- index (1).js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-const UserModel = require('./user');
-
-const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/appdb';
-const sequelize = new Sequelize(databaseUrl, { logging: false });
-
-const User = UserModel(sequelize);
-
-module.exports = { sequelize, User };
